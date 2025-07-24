f19b8ba13f61c9bf436470a6a6efda7b897707f7
Doğanay Bozkurt <140709366+thebzkurt@users.noreply.github.com>
2025-07-24 09:06:39 +0300

    adding .env

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import  requests
+import requests
+from dotenv import load_dotenv
+import os
 from Tools.scripts.pathfix import preserve_timestamps
 
 #Step 1: API SETUP
-API_KEY = "a19949006367bbf29bc47a3d432bf3ef"
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+BASE_URL_DAYS = "https://api.openweathermap.org/data/2.5/forecast/daily"
 
 #Step 2: Get weather data
 def get_weather(city):
@@ -27,6 +31,11 @@ def get_weather(city):
     except Exception as e:
         print("An error occurred: ", e)
         return None
+
+def get_days_weather(city):
+    try:
+        url = f"{BASE_URL_DAYS}?q={city}&appid={API_KEY}&units=metric"
+
 
 #Step 3: Display weather information
 def display_weather(weather):
